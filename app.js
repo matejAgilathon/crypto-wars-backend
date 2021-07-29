8998934c23ec7cec4b264a369d9a7d17eb147b8f
matejAgilathon <matej.pavic@agilathon.com>
2021-07-29 16:43:16 +0200

    adding descriptive comment

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000
 //Middleware
 app.use(express.json())
 
+//Controllers
 app.get('/', async (req, res) => {
     let CoinGeckoUri = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20litecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d'
     try {
