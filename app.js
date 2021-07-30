782b76b19fdc2e4df4f6ee17a34cd709c95dee2f
matejAgilathon <matej.pavic@agilathon.com>
2021-07-30 15:51:20 +0200

    sigin logic and refactoring

--- app.js
+++ app.js
@@ -14,55 +14,78 @@ app.use(express.json())
 
 //Controllers
 app.get('/', async (req, res) => {
-    let CoinGeckoUri = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20litecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d'
+    let CoinGeckoUri =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20litecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d'
     try {
         const response = await axios.get(CoinGeckoUri)
         res.json(response.data)
     } catch (error) {
         console.log(error)
-        res.status(500).json({msg: "An error occured, try again later"})
+        res.status(500).json({ msg: 'An error occured, try again later' })
     }
 })
+
+let collection
+const mongoConnection = async (databaseName, collectionName) => {
+    await client.connect()
+    const db = client.db(databaseName)
+    collection = db.collection(collectionName)
+}
 
 app.post('/user/register', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         // create a document to be inserted
         const user = { name: req.body.name, password: hashedPassword }
-        await client.connect();
-        const db = client.db("crypto");
-        const collection = db.collection("users");
-        const result = await collection.updateOne({name: user.name},{$set:{password: hashedPassword}}, {upsert: true});
-        
+        await mongoConnection('crypto', 'users')
+        const result = await collection.updateOne(
+            { name: user.name },
+            { $set: { password: hashedPassword } },
+            { upsert: true }
+        )
+
         if (result.upsertedCount === 0) {
-            res.json({msg: 'User Already exists'})
+            res.json({ msg: 'User Already exists' })
         } else {
-            res.json({msg: 'Welcome to crypto world!'})
+            res.json({ msg: 'Welcome to crypto world!' })
         }
         console.log(result)
     } catch (error) {
-        res.json({msg: "Server Error"})
+        res.json({ msg: 'Server Error' })
         console.log(error)
     } finally {
-        await client.close();
+        await client.close()
     }
 })
 
 app.post('/user/signin', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name)
-    if (!user) {
-        res.status(400).send("User not found")
-    }
     try {
-        if(await bcrypt.compare(req.body.password, user.password)) {
-            res.send("Success")
-        } else {
-            res.send("Not allowed")
+        await mongoConnection('crypto', 'users')
+        // Query for a user that has the name of req.body.name
+        const query = { name: req.body.name }
+        const options = {
+            // Include only the `name` and `password` fields in the returned document
+            projection: { name: 1, password: 1 },
+        }
+        const user = await collection.findOne(query, options)
+        // user validation logic with bcrypt
+        if (!user) {
+            res.status(400).json({ msg: 'User not found' })
+        }
+        try {
+            if (await bcrypt.compare(req.body.password, user.password)) {
+                res.json({ msg: 'Success' })
+            } else {
+                res.send({ msg: 'Not allowed' })
+            }
+        } catch (error) {
+            res.status(500).json({ msg: 'An error occured, try again later' })
         }
     } catch (error) {
-        res.status(500).json({msg: "An error occured, try again later"})
+        res.status(500).json({ msg: 'An error occured, try again later' })
+    } finally {
+        await client.close()
     }
 })
 
-
-app.listen(PORT, () => console.log('app is listening on a port 3000'))
+app.listen(PORT, () => console.log('app is listening on a port 5000'))
--- db-connection.js
+++ db-connection.js
@@ -1,22 +1,25 @@
 import 'dotenv/config'
 
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb'
 // Connection URI
-const uri = process.env.DB_CONNECT;
+
+const uri = process.env.DB_CONNECT
 // Create a new MongoClient
-const client = new MongoClient(uri);
-let db
+const client = new MongoClient(uri)
+
 async function run() {
-  try {
+    try {
     // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    db = await client.db("crypto").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
+        await client.connect()
+        // Establish and verify connection
+        await client.db('crypto').command({ ping: 1 })
+        console.log('Connected successfully to server')
+    }catch(error) {
+        console.log(error)
+    } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+        await client.close()
+    }
 }
 
-run().catch(console.dir);
+run().catch(console.dir)
