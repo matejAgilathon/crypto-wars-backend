275bb36fb1748ced0384b71b9067787410dd7e50
matejAgilathon <matej.pavic@agilathon.com>
2021-08-26 11:56:22 +0200

    console log request

--- check-auth.js
+++ check-auth.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken'
 
 const checkAuth =  (req, res, next) => {
     try {
+        console.log('headers',req.headers.authorization)
+        console.log('body',req.body)
         const token = req.headers.authorization.split(' ')[1]
         const decode = jwt.verify(token, 'lozincica123')
         req.decodedData = decode
